620ef2efb0b6a7710028870966edbae3b4792c8e
Иван Чернядьев <chim20r077@student.bmstu.ru>
2023-09-18 18:14:25 +0300

    Add completed project

---
+++ diplom_prj/media/results/alisa_id1/task-1_id4/task-1-id4.js
@@ -0,0 +1,40 @@
+function divide(dividend, divisor) {
+    if (divisor === 0) {
+      throw new Error("Error: Division by zero is not allowed.");
+    }
+  
+    return dividend / divisor;
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
