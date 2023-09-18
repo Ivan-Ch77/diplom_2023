a592e9baef5b9622303e88bec57afb78d8e4ca68
Иван Чернядьев <chim20r077@student.bmstu.ru>
2023-09-18 18:19:06 +0300

    Revert "Add completed project"
    This reverts commit 620ef2efb0b6a7710028870966edbae3b4792c8e.

--- diplom_prj/tasks/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-from .views import *
-
-urlpatterns = [
-  path('', taskspage, name='tasks'),
-  path('task-id<int:id>/', task, name='task'),
-  path('task-id<int:id>/save', save_result, name='save_task_result'),
-]
